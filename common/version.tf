9b396335c32c525aa081e90b7863ca8611762603
Chris Germany <Chris.Germany@blueyonder.co.uk>
2021-06-23 21:14:44 +0000

    Add resource group to version.tf for backend access

--- common/version.tf
+++ common/version.tf
@@ -1,4 +1,4 @@
-#` Version of Terraform Code to be user for this project
+# Version of Terraform Code to be user for this project
 
 terraform {
   required_providers {
@@ -8,11 +8,11 @@ terraform {
     }
   }
 
-#  backend "azurerm" {
-#      storage_account_name = "n127cbpstortistate"
-#      container_name       = "tfstate"
-#      key                  = "common.terraform.tfstate"
-#      access_key           = "5xRvNclmoGgRtze20rBWq2eANgMellkerpVp6f7/68QfVTL8VOGHDPDwibOLNTLr+kG/gv2P3xLQ0odN243Jlg=="
-#  }
+  backend "azurerm" {
+      resource_group_name  = "GhostTerraformRg"
+      storage_account_name = "ghostterraformstate"
+      container_name       = "tfstate"
+      key                  = "common.terraform.tfstate"
+  }
 }
 
