6dfa265fced87c8c2f1adbc88e68e5aea56af51b
Chris Germany <Chris.Germany@icloud.com>
2021-07-14 08:55:35 +0100

    Add additional database outputs

--- database/output.tf
+++ database/output.tf
@@ -7,3 +7,12 @@ output "mysql_database" {
     value = module.database.mysql_database
     sensitive = true
 }
+
+output "mysql_server_name" {
+    value = module.database.mysql_server.name
+}
+
+output "mysql_database_name" {
+    value = module.database.mysql_database.name
+}
+
