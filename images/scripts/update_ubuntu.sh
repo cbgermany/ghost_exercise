9741919c68b4602e6bed316390df3a9005424e62
Chris Germany <Chris.Germany@icloud.com>
2021-07-15 12:40:23 +0100

    Add sendmail

--- images/scripts/update_ubuntu.sh
+++ images/scripts/update_ubuntu.sh
@@ -14,3 +14,11 @@ then
     echo "Error: Failed to upgrade the Ubuntu updates, configuration will now stop"
     exit 1
 fi
+
+# Install sendmail as this will be used to send email invites
+sudo apt install sendmail -y
+if [[ $? != 0 ]]
+then
+    echo "Error: Failed to install sendmail, configuration will now stop"
+    exit 1
+fi
